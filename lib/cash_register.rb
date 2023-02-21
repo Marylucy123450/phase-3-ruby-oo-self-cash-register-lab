830d16faf2a5a04ce39d2098f823aaac18c5343e
Marylucy <marylucyatienoomenda.gmail.com>
2023-02-21 12:36:58 +0300

    done

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,12 +1,5 @@
 class CashRegister
 
-  attr_accessor :total, :items, :last_item
-  attr_reader :discount
-  def initialize(discount=0)
-      @discount = discount
-      @total = 0 
-      @items = []     
-  end
 
   # sumbmitting changes
 
